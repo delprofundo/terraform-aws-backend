1020130cfec59e64755850b5f2829253fa3c2851
Bruno Watt <bruno@hypermedia.tech>
2021-09-04 22:07:07 +1000

    fix sse

--- main.tf
+++ main.tf
@@ -47,38 +47,34 @@ resource "aws_s3_bucket" "s3_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        rule {
-          apply_server_side_encryption_by_default {
-            sse_algorithm    = "aws:kms"
-            kms_master_key_id = aws_kms_key.kms_key.arn
-          }
-        }
-      }
-      tags = {
-        ResourceGroup = local.namespace
+        sse_algorithm = "aws:kms"
+        kms_master_key_id = aws_kms_key.kms_key.arn
       }
     }
+  }
+  tags = {
+    ResourceGroup = local.namespace
+  }
+}
 
-    resource "aws_s3_bucket_public_access_block" "s3_bucket" {
-      bucket = aws_s3_bucket.s3_bucket.id
+resource "aws_s3_bucket_public_access_block" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
-      block_public_acls       = true
-      block_public_policy     = true
-      ignore_public_acls      = true
-      restrict_public_buckets = true
-    }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
-    resource "aws_dynamodb_table" "dynamodb_table" {
-      name          = "${local.namespace}-state-lock"
-      hash_key      = "LockID"
-      billing_mode  = "PAY_PER_REQUEST"
-      attribute {
-        name = "LockID"
-        type = "S"
-      }
-      tags = {
-        ResourceGroup = local.namespace
-      }
-    }
+resource "aws_dynamodb_table" "dynamodb_table" {
+  name          = "${local.namespace}-state-lock"
+  hash_key      = "LockID"
+  billing_mode  = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    ResourceGroup = local.namespace
   }
 }
